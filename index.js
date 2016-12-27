3622e9fe41e56e617c4cf05d10f002b0ab695343
Iain Reid <iainreid382@gmail.com>
2016-12-27 14:48:36 +0000

    [chore] Rotations fixed

--- index.js
+++ index.js
@@ -3,80 +3,55 @@
 // Lib
 const lib = require('./lib');
 
-console.log(lib.moves.length)
+console.log(lib.moves.length);
 
 const deadends = [];
 
-// (function attemptSolution() {
-//   // Bootstrap the cube
-//   const cube = bootstrapCube();
+(function attemptSolution() {
+  // Generate the cube
+  const cube = lib.methods.generateCube();
 
-//   // Setup some scoped variables
-//   let possibleMoves = [];
+  // Setup some scoped variables
+  let possibleMoves = [];
 
-//   while (calculatePossibleMoves()) {
-//     let move = retrieveRandomIndex(availableMoves);
+  while (calculatePossibleMoves()) {
+    let move = retrieveRandomIndex(availableMoves);
 
-//     for (let co of move.coordinates) {
-//       cube[co.x.toString() + co.y.toString() + co.z.toString()] = false;
-//     }
+    for (let co of move.coordinates) {
+      cube[co.x.toString() + co.y.toString() + co.z.toString()] = false;
+    }
 
-//     moves.push(move);
-//   }
+    moves.push(move);
+  }
 
-//   function bootstrapCube() {
+  function calculatePossibleMoves() {
+    // Clear the previously possible moves
+    possibleMoves.length = 0;
 
-//   }
+    for (let piece of pieces) {
+      if (!piece.hasBeenPlaced) {
+        let m = piece.getAvailableMoves();
 
-//   function calculatePossibleMoves() {
-//     // Clear the previously possible moves
-//     possibleMoves.length = 0;
+        if (!m.length) {
+          handleFailure();
+          return false;
+        } else {
+          possibleMoves.push(...m);
+        }
+      }
+    }
 
-//     for (let piece of pieces) {
-//       if (!piece.hasBeenPlaced) {
-//         let m = piece.getAvailableMoves();
+    return true;
+  }
 
-//         if (!m.length) {
-//           handleFailure();
-//           return false;
-//         } else {
-//           possibleMoves.push(...m);
-//         }
-//       }
-//     }
+  function handleBlockage() {
+    this.attempts = this.attempts || 1;
+    conosle.log('%s attempts', this.attempts++);
 
-//     return true;
-//   }
+    // Mark the point as impassable
+    deadends.push(hashMoves);
 
-//   function retrieveRandomIndex(arr) {
-//     return arr[Math.floor(Math.random() * arr.length)];
-//   }
-
-//   function generateMoveHistoryHash(...moves) {
-//     return moves.map(move => move.hash);
-//   }
-
-//   /**
-//    * Generate the available points within the cube
-//    */
-//   function bootstrapCube() {
-//     let cube = {};
-
-//     for (let i = 1, j = 1, k = 1; k <= 4; ++i > 4 && (i = 1, ++j > 4 && (j = 1, k++))) {
-//       cube[i.toString() + j.toString() + k.toString()] = true;
-//     }
-
-//     return cube;
-//   }
-
-//   function handleBlockage() {
-//     this.attempts = this.attempts || 1;
-//     conosle.log('%s attempts', this.attempts++);
-
-//     // Mark the point as impassable
-//     deadends.push(hashMoves);
-
-//     // Queue another solution attempt
-//     setTimeout(attemptSolution);
-//   }
-// })();
+    // Queue another solution attempt
+    setTimeout(attemptSolution);
+  }
+})();
